5ee33e60fd9c6110ed54fccebd9abc9b5a9fe2a8
Laysa Numaire <laysa_numaire@hotmail.com>
2024-01-19 13:24:54 -0300

    Jo_numero_secreto

--- Game-Js/app.js
+++ Game-Js/app.js
@@ -1,9 +1,22 @@
 alert('Boas-vindas ao jogo do número secreto');
-let numeroSecreto = 5;
-let chute = prompt('Escolha um número entre 1 e 10');
+let numeroSecreto = 8;
+console.log (numeroSecreto)
+let chute;
 
-if ( chute == numeroSecreto) {
-    console.log('Voce descobriu o número secreto! (5)');
+//enquanto o chute não for igual ao numeroSecreto...
+while (chute != numeroSecreto) {
+    chute = prompt ('Escolha um número entre 1 e 10')
+
+    //Se o chute for igual ao numero secreto faça isso...
+    if ( chute == numeroSecreto) {
+    alert (`Voce descobriu o numero secreto! (${numeroSecreto})`); //template String
+    }  else { 
+         if (chute > numeroSecreto) {
+        alert (`O numero secreto é menor que ${chute}`);
+    } else { //se a firmação for falsa execute isso...
+        alert (`O numero secreto é maior que ${chute}`)
+    }
+}
 }
 
 
