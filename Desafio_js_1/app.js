0487f01adc77068b752001ea63a81a8680898d65
Laysa Numaire <laysa_numaire@hotmail.com>
2024-01-22 21:53:41 -0300

    Modificações_22_01

---
+++ Desafio_js_1/app.js
@@ -0,0 +1,10 @@
+let titulo = document.querySelector ('h1')
+titulo.innerHTML = 'Hora do desafio';
+
+function consoleButton() {
+    console.log ('O botão foi clicado');
+}
+
+function alertButton() {
+    alert ('Eu amo JS');
+}
---
+++ js_secreto/app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo do número secreto';
+
+let paragrafor = document.querySelector('p');
+paragrafor.innerHTML = 'Escolha um número de 1 a 10.';
+
+function verificarChute() {
+    console.log ('o botão foi clicado');
+}
